12a6746b8e1f2aa7c0e7a964813994b72ae35e63
joeltonmats <joeltonmatos@gmail.com>
2018-02-02 14:03:36 -0200

    feat: initial commit fetch api introduction
    This feat comes with initial structure and the first
    fetch request.

---
+++ fetch-api-lab/js/main.js
@@ -0,0 +1,84 @@
+var app = (function () {
+    'use strict';
+
+    function logResult(result) {
+        console.log(result);
+    }
+
+    function logError(error) {
+        console.log('Looks like there was a problem: \n', error);
+    }
+
+    // Verify fetch support
+    if (!('fetch' in window)) {
+        console.log('Fetch API not found, try including the polyfill');
+        return;
+    }
+
+    function fetchJSON() {
+        fetch('examples/animals.json')
+            .then(logResult)
+            .catch(logError);
+    }
+
+    function validateResponse(response) {
+        // TODO 2.3
+    }
+
+    function readResponseAsJSON(response) {
+        // TODO 2.4
+    }
+
+    function showImage(responseAsBlob) {
+        //  TODO 3a
+    }
+
+    function readResponseAsBlob(response) {
+        // TODO 3b
+    }
+
+    function fetchImage() {
+        // TODO 3c
+    }
+
+    function showText(responseAsText) {
+        //  TODO 4a
+    }
+
+    function readResponseAsText(response) {
+        // TODO 4b
+    }
+
+    function fetchText() {
+        // TODO 4c
+    }
+
+    function headRequest() {
+        // TODO 5.1
+    }
+
+    function logSize(response) {
+        // TODO 5.2
+    }
+
+    /* NOTE: Never send unencrypted user credentials in production! */
+    function postRequest() {
+        // TODO 6.2
+    }
+
+    // Don't worry if you don't understand this, it's not part of the Fetch API.
+    // We are using the JavaScript Module Pattern to enable unit testing of
+    // our functions.
+    return {
+        readResponseAsJSON: (readResponseAsJSON),
+        readResponseAsBlob: (readResponseAsBlob),
+        readResponseAsText: (readResponseAsText),
+        validateResponse: (validateResponse),
+        fetchJSON: (fetchJSON),
+        fetchImage: (fetchImage),
+        fetchText: (fetchText),
+        headRequest: (headRequest),
+        postRequest: (postRequest)
+    };
+
+})();
